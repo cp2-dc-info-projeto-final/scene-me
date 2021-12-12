9122abaa1f90a62382347b9f3044eaa26ecc9669
RafaelDavier <86801321+RafaelDavier@users.noreply.github.com>
2021-12-11 22:29:53 -0300

    Delete userdadosTabela.sql

--- userdadosTabela.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE userdados(
-    id int(11) AUTO_INCREMENT,
-    nome varchar(15) NOT NULL,
-    email varchar(40) NOT NULL,
-    senha varchar(40) NOT NULL,
-    dataNasc dat NOT NULL),
-    adm int(11) NULL,
-    PRIMARY KEY id)
-    );
