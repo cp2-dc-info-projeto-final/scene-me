8d082b5c1982ab13171415aa7570c4fafb387808
RafaelDavier <86801321+RafaelDavier@users.noreply.github.com>
2021-12-15 20:19:06 -0300

    Update comCurtida.php

--- src/comCurtida.php
+++ src/comCurtida.php
@@ -4,15 +4,17 @@ include 'autentica.php';
 
   $curtidaPostId = $_POST['idCurtida'];
   $curtirQ = $_POST['curtir'];
-    $autorC = $_SESSION["nome"];
+  $autorC = $_SESSION["nome"];
     if($curtirQ == "sim")
     {
         $sql = "INSERT INTO curtidas (autorCommentCurtida, curtidaCommentId) VALUES ('$autorC','$curtidaPostId')";
         mysqli_query($conexao, $sql);
+        header("Location: paginaInicial.php");
     }
     else
     {
         $sql = "DELETE FROM curtidas WHERE autorCommentCurtida = '$autorC' AND curtidaCommentId =  '$curtidaPostId'";
         mysqli_query($conexao, $sql);
+        header("Location: paginaInicial.php");
     }
 ?>
