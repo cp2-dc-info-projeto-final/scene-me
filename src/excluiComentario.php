f8ab2ac64f241bfb798625d3fd0a29a4c546492f
RafaelDavier <86801321+RafaelDavier@users.noreply.github.com>
2021-12-18 21:16:25 -0300

    Delete src directory

--- src/excluiComentario.php
+++
@@ -1,9 +0,0 @@
-<?php
- 
- include 'conecta.php';
- $id = $_GET['id'];
- $sql = "DELETE FROM comentarios WHERE id = '$id'";
- $deletar = mysqli_query($conexao, $sql);
-
- header("Location: paginaInicial.php");
-?>
